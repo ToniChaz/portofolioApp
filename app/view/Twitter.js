460b171e8d142557e5193154dec3d88064bc1283
Toni Chaz <toni.chaz@hotmail.com>
2013-02-15 14:54:05 +0100

    add refresh button and twitts template

--- app/view/Twitter.js
+++ app/view/Twitter.js
@@ -1,36 +1,41 @@
+var timeline = {
+    title: 'Twitts',
+    tpl: [
+         '<div class="tweet">',
+                 '<div class="avatar"><img src="http://cdn4.iconfinder.com/data/icons/macosxstyle/macosxstyle_png/128/Finder.png" /></div>',
+                 '<div class="tweet-content">',
+                     '<h2>Mac</h2>',
+                     '<p>This is example</p>',
+                 '</div>',
+         '</div>'
+     ]
+};
+var mapPanel = {
+    title: 'Mapa',
+    html: 'Aqui encuntras el oro'
+};
 Ext.define('portofolio.view.Twitter',{
   extend: 'Ext.TabPanel',
-  xtype: 'twitter',
-  
-  
+  xtype: 'twitter',  
   
   config: {
     title:'Twitter',
     iconCls: 'twitter2',
     
-    styleHtmlContent: true,
-    
-    items: [
-        {
-            title: 'Twitts',
-            html: 'Lista de twitts'
+    styleHtmlContent: true, 
+   tabBar:{
+            dockedItems:[{ 
+                xtype: 'button',
+                iconMask: true,
+                docked: 'right',
+                ui: 'action',
+                stretch: false,
+                style: 'height:35px;margin-top:5px;',
+                handler: function(){
+                    alert('Refresh panel');    
+                }
+            }]
         },
-        {
-            title: 'Mapa',
-            html: 'Lugar de twitts'
-        },
-        {
-            xtype: 'toolbar',
-            docked: 'top',
-            defaults: {
-                iconMask: true
-            },
-            items: [
-                { xtype: 'spacer' },
-                { iconCls: 'refresh' },
-                { xtype: 'spacer' }
-            ]
+            items: [{iconCls: 'refresh'},timeline,mapPanel]
         }
-    ]
-  }
 });
